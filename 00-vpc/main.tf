124558303bab38411723d19456873f81a792b687
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-20 01:18:03 +0530

    terraform-modules

---
+++ 00-vpc/main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+    source = "git::https://github.com/gaddam-akhil/terraform-aws-vpc.git?ref=main"
+    project = var.project_name
+    environment = var.env
+    Is_peering_required = true
+}
