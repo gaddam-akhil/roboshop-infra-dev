a841faeafe092004a74cfdfbbd474c19b21e6bb5
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-20 22:09:43 +0530

    terraform-modules

--- 40-databases-instances/locals.tf
+++ 40-databases-instances/locals.tf
@@ -5,7 +5,7 @@ locals {
     Environment = var.env
     Terraform = "true"
   }
-  database_subnet_id = data.aws_ssm_parameter.database_subnet_ids.value
+  database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_ids.value
 }
 
