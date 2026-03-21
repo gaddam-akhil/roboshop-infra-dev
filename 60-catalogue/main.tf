47ec1d6eb5ca9d4e09e75e34a09fb01ec8d185aa
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-22 00:34:11 +0530

    terraform-modules

---
+++ 60-catalogue/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "catalogue" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id =  local.database_subnet_id
+  vpc_security_group_ids =[local.mongodb_sg_id]
+  #iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+  tags = merge(
+    {
+        Name = "${var.project_name}-${var.env}-mongodb"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "bootstrap-mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+ connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+ provisioner "file" {
+  source = "bootstrap.sh"
+  destination = "/tmp/bootstrap.sh"
+ }
+
+ provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh", #giving excute access
+        "sudo sh /tmp/bootstrap.sh mongodb"
+    ]
+  }
+}
