5725b6c77160711895a12b9bb2df97e5d7de61b3
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-22 11:37:34 +0530

    terraform-modules

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -38,8 +38,15 @@ resource "terraform_data" "bootstrap-catalogue" {
   }
 }
 
-/* resource "aws_ec2_instance_state" "catalogue" {
+resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state       = "stopped"
-} */
+   depends_on = [terraform_data.bootstrap-catalogue]
+} 
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project_name}-${var.env}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [aws_ec2_instance_state.catalogue]
+}
 
