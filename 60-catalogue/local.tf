ec466ba15adf46602ca1011311e1943dff372c02
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-22 11:39:44 +0530

    terraform-modules

--- 60-catalogue/local.tf
+++ 60-catalogue/local.tf
@@ -1,7 +1,7 @@
 locals {
   ami_id = data.aws_ami.amidevops.id
-  catalogue_subnet_id = split(",",data.aws_ssm_parameter.catalogue_subnet_ids.value)[0]
-  private_sg_id = data.aws_ssm_parameter.private_sg_id.value
+  private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   common_tags = {
     Project = var.project_name
     Environment = var.env
