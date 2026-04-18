d7296d91be9b8c3433ebcb3341effc37f9dd3c6d
gaddam akhil <gaddamakhil900@gmail.com>
2026-04-18 15:50:31 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,5 @@
 module "component" {
-    for_each = var.component
+    for_each = var.components
   source = "git::https://github.com/gaddam-akhil/terraform-roboshop-components.git?ref=main"
   component = each.key
   rule_priority = each.value.rule_priority
