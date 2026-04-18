47ac81b8ca9ccde1336aa80773944833f41db3e1
gaddam akhil <gaddamakhil900@gmail.com>
2026-04-18 10:48:02 +0530

    terraform

--- 90-components/variable.tf
+++ 90-components/variable.tf
@@ -4,18 +4,18 @@ variable "component" {
         catalogue = {
             rule_priority = 10
         }
-         user = {
-            rule_priority = 20
-        }
-           cart = {
-            rule_priority = 30
-        }
-          shipping = {
-            rule_priority = 40
-        }
-        payment = {
-            rule_priority = 50
-        }
+        #  user = {
+        #     rule_priority = 20
+        # }
+        #    cart = {
+        #     rule_priority = 30
+        # }
+        #   shipping = {
+        #     rule_priority = 40
+        # }
+        # payment = {
+        #     rule_priority = 50
+        # }
         # frontend attaching  to frontend alb
             frontend = {
             rule_priority = 10
